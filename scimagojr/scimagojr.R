f760959f68e329a521eec42df65ee9620a4ed9cf
FX Jollois <fxjolloisupd@gmail.com>
2021-03-18 17:10:00 +0100

    scimagojr complete update 2019

--- scimagojr/scimagojr.R
+++ scimagojr/scimagojr.R
@@ -3,17 +3,18 @@ library(stringr)
 library(dplyr)
 library(xlsx)
 
-fichier = dir(pattern = "scimagojr [0-9]+.xlsx")
+fichier = dir(pattern = "scimagojr country rank [0-9]+.xlsx")
 
 liste = lapply(fichier, function(f) {
-  annee = str_split(str_split(f, " ")[[1]][2], "\\.")[[1]][1]
+  annee = str_extract(f, "[0-9]+")
+  cat("Année : ", annee, "\n")
   test = cbind(Year = as.numeric(annee), read_excel(f))
   return(test)
 })
 
 res = Reduce(function(a, b) {
   return(bind_rows(a, b))
-}, liste[1:24])
+}, liste)
 
 write.xlsx(res, "scimagojr.xlsx", sheetName = "All",
            col.names = TRUE, row.names = FALSE, append = FALSE)
